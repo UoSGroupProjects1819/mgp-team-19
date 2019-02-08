133419652d31852c3ab72c387f5174ed2e5f729f
Fred-Wright <fredwright2000@live.co.uk>
2019-02-08 13:58:18 +0000

    Initial movement+camera

---
+++ Group19Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 12f;
+
+
+    void FixedUpdate()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+}
