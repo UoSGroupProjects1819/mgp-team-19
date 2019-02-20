6e217272e3145fc4a3010b3334b185465088c9a2
Fred-Wright <fredwright2000@live.co.uk>
2019-02-20 00:05:37 +0000

    Random things, particles, game management

---
+++ Group19Game_Fred/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+    public static bool paused = false;
+    public GameObject PauseUI;
+
+    //acts as an escape menu, when esc is pushed, time freezes and menu shows
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+        
+        
+    }
+
+    public void Paused()
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void Resume()
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+
+    }
+
+
+
+
+}
