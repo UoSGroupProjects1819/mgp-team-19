8269b11219e5d4cd6542650d653031e70c7af168
fred-wright <fredwright2000@live.co.uk>
2019-03-06 16:23:26 +0000

    Elevators

---
+++ Group19Game_Fred/Assets/Scripts/Elevator/ElevatorDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDown : MonoBehaviour
+{
+    public GameObject lift;
+    public float liftSpeed = 12f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "SecondPlayer")
+        {
+            if (ElevatorUp.DoorMoveVert >= 0)
+            {
+                lift.transform.Translate(Vector3.down * (Time.deltaTime * liftSpeed));
+                ElevatorUp.DoorMoveVert -= 1;
+            }
+        }
+    }
+}
---
+++ Group19Game_Fred/Assets/Scripts/Elevator/ElevatorFoward.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorFoward : MonoBehaviour
+{
+    public GameObject lift;
+    public float liftSpeed = 12f;
+    public float maxDoorMove3d = 100f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "SecondPlayer")
+        {
+            if (ElevatorBack.DoorMove3d >= -maxDoorMove3d)
+            {
+                lift.transform.Translate(Vector3.forward * (Time.deltaTime * liftSpeed));
+                ElevatorBack.DoorMove3d -= 1;
+            }
+        }
+    }
+}
