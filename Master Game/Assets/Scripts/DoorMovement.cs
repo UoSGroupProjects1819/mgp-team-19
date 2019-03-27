57b780f08c17ec0e22f11d3f8ae2e9181a7235c0
tobulls <35865917+tobulls@users.noreply.github.com>
2019-03-27 14:58:54 +0000

    Lots of stuff
    too much stuff for me to say seperately consisting of:
    adding master level, being a merge of all
    added multiple asset packs

---
+++ Master Game/Assets/Scripts/DoorMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+    public Material _default;
+    public Material newMaterial;
+
+    public float moveSpeedMultiplier = 8;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnMouseOver()
+    {
+        GetComponent<Renderer>().material = newMaterial;
+
+        if(Input.GetMouseButton(0))
+        {
+            rb.useGravity = false;
+
+            this.gameObject.transform.Translate(Vector3.up * (Time.deltaTime * moveSpeedMultiplier));
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            rb.useGravity = true;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        rb.useGravity = true;
+        GetComponent<Renderer>().material = _default;
+    }
+}
